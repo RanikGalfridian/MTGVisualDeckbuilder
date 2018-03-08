f5cf7eb8abb9fc3af413fe262592fc1535867c18
RanikGalfridian <zach.hackett1@gmail.com>
2018-03-07 23:27:41 -0500

    Add styling to create and load buttons

--- mtg-vdb/main.js
+++ mtg-vdb/main.js
@@ -21,7 +21,7 @@ const {app, BrowserWindow, Menu} = require('electron')
     }))
   
     // Open the DevTools.
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
   
     // Emitted when the window is closed.
     win.on('closed', () => {
