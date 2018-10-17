e2f6b56ca40a158c4d4bd33985b0dcfa058f74f8
Zach <zhackett@stevens.edu>
2018-10-16 23:45:04 -0400

    Adjust create_span

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,10 @@
 const {app, BrowserWindow} = require('electron').remote
 window.$ = window.jQuery = require('jquery')
+
 function closeWindow() {
   var window = BrowserWindow.getFocusedWindow();
   window.close();
 }
-
-$("")
 
 document.onreadystatechange = () => {
   if (document.readState == "complete") {
